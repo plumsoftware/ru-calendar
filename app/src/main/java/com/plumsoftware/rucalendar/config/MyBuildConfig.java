b45427d87f995814dfd8f65541982c3cc93b4bfb
Slava <plumsoftwareofficial@gmail.com>
2025-12-14 22:19:08 +0600

    Добавил метрику

--- app/src/main/java/com/plumsoftware/rucalendar/config/MyBuildConfig.java
+++ app/src/main/java/com/plumsoftware/rucalendar/config/MyBuildConfig.java
@@ -8,6 +8,7 @@ public final class MyBuildConfig {
     public static final int VERSION_CODE = 1;
     public static final String VERSION_NAME = "1.0.0";
 
+    public static String RUSTORE_APP_METRICA_API_KEY = "640dd7c7-f297-41c0-8337-df72654ccf8f";
     public MyBuildConfig() {
     }
 }
